903c67a142af1d01be2642cf7c00e675856f2899
Abilash Venkatesan <abilashvenkat6190@gmail.com>
2021-07-07 16:08:43 -0400

    Added promo component

---
+++ src/components/Promo.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Promo = () => {
+    return (
+        <div>
+            <header className="promo">
+                <h1>Summer Sale is live now!!!</h1>
+            </header>
+        </div>
+    )
+}
+export default Promo
