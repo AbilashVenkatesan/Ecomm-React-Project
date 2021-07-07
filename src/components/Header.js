bf0fd7ac1674159afb8151b0518d45a0759a951e
Abilash Venkatesan <abilashvenkat6190@gmail.com>
2021-07-07 16:34:57 -0400

    Minor changes in header file

--- src/components/Header.js
+++ src/components/Header.js
@@ -24,8 +24,8 @@ const Header = () => {
 							<li><a href="#">Sale</a></li>
 						</ul>
 					</li>
-					<li><a href="#">About Us</a></li>
-					<li><a href="#">Contact Us</a></li>
+					<li><a href="About Us">About Us</a></li>
+					<li><a href="Contact Us">Contact Us</a></li>
 				</ul>
 			</nav>
 		
