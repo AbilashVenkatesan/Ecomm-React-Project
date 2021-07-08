bd32e4a2df711451c9e6c8ce55cafb56b0e9bcb5
Abilash Venkatesan <abilashvenkat6190@gmail.com>
2021-07-08 15:07:45 -0400

    reference changes in prod array and app.js

--- src/components/ProdArray.js
+++ src/components/ProdArray.js
@@ -8,16 +8,17 @@ const ProdArray = ({data}) => {
         <article className="toy">
             <header>
                 <div className="thumbnail">
-                    <a href="#"><img src={img} alt={`{toyImg}`}/></a>
+                    <a href="product.html"><img src={img} alt={`{toyImg}`}/></a>
                 </div>
-                <h1>{toyName}</h1>
+                <h1><a href="product.html">{toyName}</a></h1>
                 <h2>Description:</h2> 
                 <p>{toyDescription}</p>
                 <data value="39"><del>${actualPrice}</del> <ins>${offerPrice}</ins></data>
             </header>
             <footer>
-                <button type="button" class="wishlist"><span class="material-icons" aria-label="Favourites"></span>wishlist</button>
-                <button type="button" class="your-cart"><span class="material-icons" aria-label="Add to bag"></span>your-cart</button>
+                <button type="button" class="view-product"><a href="product.html">View Product</a></button>
+                <button type="button" class="wishlist"><span class="material-icons" aria-label="Favourites"></span>Add to Wishlist</button>
+                <button type="button" class="your-cart"><span class="material-icons" aria-label="Add to bag"></span>Add to Cart</button>
             </footer>   
         </article>
     )
