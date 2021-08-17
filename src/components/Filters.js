3a6fec63b3a66f8eef83533ea05b07fbab6e81c1
Abilash Venkatesan <abilashvenkat6190@gmail.com>
2021-08-17 12:36:26 -0400

    changed filter options

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,89 +1,89 @@
 import React from 'react'
 const Filters = () => {
 
+
+  // filter-form
   return (
-    <div class="filter-options">
-        <a><h2>Filter</h2></a>
-        <fieldset className="search">
-			<input type="search" name="search" id="filterResults" autocomplete="off" />
-		</fieldset>
+    <form className="filters">
+    <h2>Filters</h2>
 
-        <fieldset>
-            <legend>For Age</legend>
-            <ol class="filter-list">
-              <li><input type="checkbox" name="age" value="2-5" id="2-5"> <label for="2-5">2-5</label></input></li>
-              <li><input type="checkbox" name="age" value="5-8" id="5-8"> <label for="5-8">5-8</label></input></li>
-              <li><input type="checkbox" name="age" value="8-12" id="8-12"> <label for="8-12">8-12</label></input></li>
-              <li><input type="checkbox" name="age" value="12-15" id="12-15"> <label for="12-15">12-15</label></input></li>
-              <li><input type="checkbox" name="age" value="15-18" id="15-18"> <label for="15-18">15-18</label></input></li>
-              </ol>
-          </fieldset>
-          <fieldset>
-            <legend>Colour</legend>
-            <ul class="filter-list">
-              <li><input type="checkbox" name="colour" value="black" id="black"> <label for="black">Black</label></input></li>
-              <li><input type="checkbox" name="colour" value="white" id="white"> <label for="white">White</label></input></li>
-              <li><input type="checkbox" name="colour" value="yellow" id="yellow"> <label for="yellow">Yellow</label></input></li>
-              <li><input type="checkbox" name="colour" value="pink" id="pink"> <label for="pink">Pink</label></input></li>
-              <li><input type="checkbox" name="colour" value="blue" id="blue"> <label for="blue">Blue</label></input></li>
-            </ul>
-          </fieldset>
-          <fieldset>
-            <legend>Sizes</legend>
-            <ol class="filter-list">
-              <li><input type="checkbox" name="size" value="xs" id="xs"> <label for="xs">XS</label></input></li>
-              <li><input type="checkbox" name="size" value="sm" id="sm"> <label for="sm">S</label></input></li>
-              <li><input type="checkbox" name="size" value="md" id="md"> <label for="md">M</label></input></li>
-              <li><input type="checkbox" name="size" value="lg" id="lg"> <label for="lg">L</label></input></li>
-              <li><input type="checkbox" name="size" value="xl" id="xl"> <label for="xl">XL</label></input></li>
-            </ol>
-          </fieldset> 
-          <fieldset>
-            <legend>Ratings (above)</legend>
-            <ol class="filter-list">
-              <li>
-                <input type="radio" name="rating" value="4" id="aboveFour">
-                <label for="aboveFour">
-                  <span class="material-icons">star</span><span class="material-icons">star</span><span class="material-icons">star</span><span class="material-icons">star</span><span class="material-icons">star_border</span>
-                </label>
-                </input>
-              </li>
-              <li>
-                <input type="radio" name="rating" value="3" id="aboveThree">
-                <label for="aboveThree">
-                  <span class="material-icons">star</span><span class="material-icons">star</span><span class="material-icons">star</span><span class="material-icons">star_border</span><span class="material-icons">star_border</span>
-                </label>
-                </input>
-              </li>
-              <li>
-                <input type="radio" name="rating" value="2" id="aboveTwo">
-                <label for="aboveTwo">
-                  <span class="material-icons">star</span><span class="material-icons">star</span><span class="material-icons">star_border</span><span class="material-icons">star_border</span><span class="material-icons">star_border</span>
-                </label>
-                </input>
-              </li>
-              <li>
-                <input type="radio" name="rating" value="1" id="aboveOne">
-                <label for="aboveOne">
-                  <span class="material-icons">star</span><span class="material-icons">star_border</span><span class="material-icons">star_border</span><span class="material-icons">star_border</span><span class="material-icons">star_border</span>
-                </label>
-              </li>
-            </ol>
-          </fieldset>
-        </div>
-        <fieldset>
-          <label for="sort">Show</label>
-          <select name="sort" id="sort" class="sort">
-            <option value="price-high">Price, highest to lowest</option>
-            <option value="price-low">Price, lowest to highest</option>
-            <option value="newest">Newest releases</option>
-            <option value="newest">Trending</option>
-          </select>
-        </fieldset>
-      </form>
+    <div className="filter-options">
+      {/* filter-options for age */}
+      <fieldset>
+        <legend className="filtertype">Age</legend>
+        <ul className="filter-list">
+          <li><input type="checkbox" name="age" value="toddler" id="toddler"/> <label htmlFor="toddler">Toddler</label></li>
+          <li><input type="checkbox" name="age" value="child" id="child"/> <label htmlFor="child">Child</label></li>
+          <li><input type="checkbox" name="age" value="kids" id="kids"/> <label htmlFor="kids">Kids</label></li>
+          <li><input type="checkbox" name="age" value="teen" id="teen"/> <label htmlFor="teens">Teens</label></li>
+        </ul>
+      </fieldset>
+
+      {/* filter-options for color */}
+      <fieldset>
+        <legend className="filtertype">Color</legend>
+        <ol className="filter-list">
+          <li><input type="checkbox" name="color" value="black" id="black"/> <label htmlFor="black">Black</label></li>
+          <li><input type="checkbox" name="color" value="white" id="white"/> <label htmlFor="white">White</label></li>
+          <li><input type="checkbox" name="color" value="yellow" id="yellow"/> <label htmlFor="yellow">Yellow</label></li>
+          <li><input type="checkbox" name="color" value="pink" id="pink"/> <label htmlFor="pink">Pink</label></li>
+          <li><input type="checkbox" name="color" value="blue" id="blue"/> <label htmlFor="blue">Blue</label></li>
+        </ol>
+      </fieldset>
+
+      {/* filter-options for ratings */}
+      <fieldset>
+        <legend className="filtertype">Ratings: </legend>
+        <ol className="filter-list">
+          {/*4 star */}
+          <li>
+            <input type="radio" name="rating" value="4" id="aboveFour"/>
+            <label htmlFor="aboveFour">
+              <span className="material-icons">star</span><span className="material-icons">star</span><span className="material-icons">star</span><span className="material-icons">star</span><span className="material-icons">star_border</span>
+            </label>
+          </li>
+
+          {/* 3 star */}
+          <li>
+            <input type="radio" name="rating" value="3" id="aboveThree"/>
+            <label htmlFor="aboveThree">
+              <span className="material-icons">star</span><span className="material-icons">star</span><span className="material-icons">star</span><span className="material-icons">star_border</span><span className="material-icons">star_border</span>
+            </label>
+          </li>
+
+          {/* 2 star */}
+          <li>
+            <input type="radio" name="rating" value="2" id="aboveTwo"/>
+            <label htmlFor="aboveTwo">
+              <span className="material-icons">star</span>
+              <span className="material-icons">star</span>
+              <span className="material-icons">star_border</span><span className="material-icons">star_border</span><span className="material-icons">star_border</span>
+            </label>
+          </li>
+
+          {/* 1 star */}
+          <li>
+            <input type="radio" name="rating" value="1" id="aboveOne"/>
+            <label htmlFor="aboveOne">
+              <span className="material-icons">star</span><span className="material-icons">star_border</span><span className="material-icons">star_border</span><span className="material-icons">star_border</span><span className="material-icons">star_border</span>
+            </label>
+          </li>
+        </ol>
+      </fieldset>
     </div>
-    
-    )
-  }
 
-  export default Filters
+    {/* sorting filter */}
+    <fieldset>
+      <label htmlFor="sort" className="filtertype">Sort</label>
+      <select name="sort" id="sort">
+        <option value="newest">Newest releases</option>
+        <option value="price-high">Price: Highest to Lowest</option>
+        <option value="price-low">Price: Lowest to Highest</option>
+      </select>
+    </fieldset>
+  </form>
+  )
+
+}
+
+export default Filters
