a5d695e78edcd9549c5222e72dc3b17eba5dc743
Abilash Venkatesan <abilashvenkat6190@gmail.com>
2021-08-18 03:52:58 -0400

    firebase connectes succesfully

---
+++ src/util/firebase.js
@@ -0,0 +1,14 @@
+import firebase from "@firebase/app";
+import "@firebase/firestore"
+
+	firebase.initializeApp({
+		apiKey: "AIzaSyCMb68Hsat8SrKKYNEsO2bddpWLvXr1-L4",
+    	authDomain: "ecomm-react-project.firebaseapp.com",
+    	projectId: "ecomm-react-project",
+    	storageBucket: "ecomm-react-project.appspot.com",
+    	messagingSenderId: "433697007672",
+    	appId: "1:433697007672:web:ce4192cf2547043a9d1ecd",
+    	measurementId: "G-5FWSJHZVQP"
+		});
+
+export default firebase
