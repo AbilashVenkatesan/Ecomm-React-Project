fe73152672683a7941c8fa902304e065c3b6f297
Abilash Venkatesan <abilashvenkat6190@gmail.com>
2021-08-19 09:27:15 -0400

    added refernce to new viewport css files in index

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+//import style files
 import 'css/index.css'
-import 'css/index.css'
+import 'css/reset.css'
+import 'css/medium.css'
+import 'css/large.css'
+import 'css/extralarge.css'
 import App from './components/App'
 
 ReactDOM.render(<App />, document.getElementById('root'))
