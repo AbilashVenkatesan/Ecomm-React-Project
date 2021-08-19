b5442c843c938114db477e1e15ffa6b0b57b2412
Abilash Venkatesan <abilashvenkat6190@gmail.com>
2021-08-19 02:51:26 -0400

    search filter integrated with filter and filter function updated

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,89 +1,67 @@
+//import files
 import React from 'react'
-const Filters = () => {
+import 'css/index.css'
+import 'css/reset.css'
+//import searchicon png
+import SearchIcon from '../img/search.png'
 
+const Filters = ({filters, setFilters}) => {  
 
-  // filter-form
-  return (
-    <form className="filters">
-    <h2>Filters</h2>
+  //age filter
+  const Handleage = (event) => {
+    setFilters({
+      ...filters,
+      age: event.target.value
+    })
+  }
 
-    <div className="filter-options">
-      {/* filter-options for age */}
-      <fieldset>
-        <legend className="filtertype">Age</legend>
-        <ul className="filter-list">
-          <li><input type="checkbox" name="age" value="toddler" id="toddler"/> <label htmlFor="toddler">Toddler</label></li>
-          <li><input type="checkbox" name="age" value="child" id="child"/> <label htmlFor="child">Child</label></li>
-          <li><input type="checkbox" name="age" value="kids" id="kids"/> <label htmlFor="kids">Kids</label></li>
-          <li><input type="checkbox" name="age" value="teen" id="teen"/> <label htmlFor="teens">Teens</label></li>
-        </ul>
-      </fieldset>
+	return(
 
-      {/* filter-options for color */}
-      <fieldset>
-        <legend className="filtertype">Color</legend>
-        <ol className="filter-list">
-          <li><input type="checkbox" name="color" value="black" id="black"/> <label htmlFor="black">Black</label></li>
-          <li><input type="checkbox" name="color" value="white" id="white"/> <label htmlFor="white">White</label></li>
-          <li><input type="checkbox" name="color" value="yellow" id="yellow"/> <label htmlFor="yellow">Yellow</label></li>
-          <li><input type="checkbox" name="color" value="pink" id="pink"/> <label htmlFor="pink">Pink</label></li>
-          <li><input type="checkbox" name="color" value="blue" id="blue"/> <label htmlFor="blue">Blue</label></li>
-        </ol>
-      </fieldset>
+		<form className="filters">
+        <h2>Filters</h2>
+        <div className="filter-options">
+          <fieldset>
+            {/* search filter */}
+            <legend>Search</legend>
+            <input type="search" value={filters.query} onChange={(event)=>{
+              setFilters({
+                ...filters,
+                query: event.target.value
+              })
+            }} />
+            <button  className="searchIcon"><img alt={"search"}src={SearchIcon} /></button>
+          </fieldset>
 
-      {/* filter-options for ratings */}
-      <fieldset>
-        <legend className="filtertype">Ratings: </legend>
-        <ol className="filter-list">
-          {/*4 star */}
-          <li>
-            <input type="radio" name="rating" value="4" id="aboveFour"/>
-            <label htmlFor="aboveFour">
-              <span className="material-icons">star</span><span className="material-icons">star</span><span className="material-icons">star</span><span className="material-icons">star</span><span className="material-icons">star_border</span>
-            </label>
-          </li>
+          {/* age filter */}
+          <fieldset>
+            <legend>Age</legend>
+              <ul className="filter-list">
+                <li><input type="radio" className="checkBoxStyle" name="age" value="all" id="all" onChange={Handleage} checked={(filters.age === `all`) ? true : false}></input><label htmlFor="all"><span></span>All Age</label></li>
+                <li><input type="radio" className="checkBoxStyle" name="age" value="toddler" id="toddler" onChange={Handleage}></input> <label htmlFor="toddler"><span></span>Toddler</label></li>
+                <li><input type="radio" className="checkBoxStyle" name="age" value="child" id="child" onChange={Handleage}></input> <label htmlFor="child"><span></span>Child</label></li>
+                <li><input type="radio" className="checkBoxStyle" name="age" value="kids" id="kids" onChange={Handleage}></input> <label htmlFor="kids"><span></span>Kids</label></li>
+                <li><input type="radio" className="checkBoxStyle" name="age" value="teens" id="teens" onChange={Handleage}></input> <label htmlFor="teens"><span></span>Teens</label></li>
+              </ul>
+            </fieldset>
+          </div>
 
-          {/* 3 star */}
-          <li>
-            <input type="radio" name="rating" value="3" id="aboveThree"/>
-            <label htmlFor="aboveThree">
-              <span className="material-icons">star</span><span className="material-icons">star</span><span className="material-icons">star</span><span className="material-icons">star_border</span><span className="material-icons">star_border</span>
-            </label>
-          </li>
-
-          {/* 2 star */}
-          <li>
-            <input type="radio" name="rating" value="2" id="aboveTwo"/>
-            <label htmlFor="aboveTwo">
-              <span className="material-icons">star</span>
-              <span className="material-icons">star</span>
-              <span className="material-icons">star_border</span><span className="material-icons">star_border</span><span className="material-icons">star_border</span>
-            </label>
-          </li>
-
-          {/* 1 star */}
-          <li>
-            <input type="radio" name="rating" value="1" id="aboveOne"/>
-            <label htmlFor="aboveOne">
-              <span className="material-icons">star</span><span className="material-icons">star_border</span><span className="material-icons">star_border</span><span className="material-icons">star_border</span><span className="material-icons">star_border</span>
-            </label>
-          </li>
-        </ol>
-      </fieldset>
-    </div>
-
-    {/* sorting filter */}
-    <fieldset>
-      <label htmlFor="sort" className="filtertype">Sort</label>
-      <select name="sort" id="sort">
-        <option value="newest">Newest releases</option>
-        <option value="price-high">Price: Highest to Lowest</option>
-        <option value="price-low">Price: Lowest to Highest</option>
-      </select>
-    </fieldset>
-  </form>
-  )
-
-}
+          {/* sort filter */}
+          <fieldset id="sortfield">
+            <label htmlFor="sort">Sort by</label>
+            <select name="sort" id="sort" defaultValue={filters.sortBy} onChange={(event)=> {
+              console.log(event.target.value)
+              setFilters({
+                ...filters,  
+                sortBy: event.target.value
+              })
+            }}>
+            {/* sort by options */}
+            <option value="none">-- Select --</option>
+            <option value="lowest">Price - lower to higher</option>
+            <option value="highest">Price - higher to lower</option>
+            </select>
+          </fieldset>
+    </form>
+	)}
 
 export default Filters
