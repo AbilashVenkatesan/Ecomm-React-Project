b8cf27c9108261c7b9bf4d614734195e08f5ab3d
Abilash Venkatesan <abilashvenkat6190@gmail.com>
2021-08-19 11:32:06 -0400

    social

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -22,9 +22,9 @@ const Footer = () => {
 
             {/* social links */}
             <ul id="contact-us" className="social">
-                <li><a href="#" className="footer-link"><img style={{width: "15"}} alt={"fb"}src={Facebook} />Facebook</a></li>
-                <li><a href="#" className="footer-link"><img style={{width: "15"}} alt={"isnta"}src={Insta} />Instagram</a></li>
-                <li><a href="#" className="footer-link"><img style={{width: "15"}} alt={"twitter"}src={Twitter} />Twitter</a></li>
+                <li><a href="#" className="footer-link"><img style={{width: "2em"}} alt={"fb"}src={Facebook} />Facebook</a></li>
+                <li><a href="#" className="footer-link"><img style={{width: "2em"}} alt={"isnta"}src={Insta} />Instagram</a></li>
+                <li><a href="#" className="footer-link"><img style={{width: "2em"}} alt={"twitter"}src={Twitter} />Twitter</a></li>
             </ul>
 
             {/* legal links */}
