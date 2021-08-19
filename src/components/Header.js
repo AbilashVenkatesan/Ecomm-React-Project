7f14fa4dd661cadfc91650bb5efb5edde54bcbda
Abilash Venkatesan <abilashvenkat6190@gmail.com>
2021-08-19 04:42:50 -0400

    classname error resolved

--- src/components/Header.js
+++ src/components/Header.js
@@ -33,8 +33,8 @@ const Header = () => {
 	
 			{/* <!-- links to wishlist and cart --> */}
 			<ul className="yourProducts">
-			<li><a href="fav"><span className="material-icons" aria-label="Favourites">favorite_border</span>Wishlist</a></li>
-			<li><a href="cart"><span className="material-icons" aria-label="Items in your cart">shopping_cart</span>Your cart</a></li>
+			<li><a href="fav" className="link"><span className="material-icons" aria-label="Favourites">favorite_border</span>Wishlist</a></li>
+			<li><a href="cart" className="link"><span className="material-icons" aria-label="Items in your cart">shopping_cart</span>Your cart</a></li>
 			</ul>
 		</header>
 	)
